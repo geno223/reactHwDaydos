cd8e3808f480fda85d21b59e8dced7b27c790480
geno223 <eugenio.jimenez22397@gmail.com>
2024-01-31 22:23:59 -0500

    Rendering movies

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,14 +1,20 @@
 const Movie = (props) => {
-
-    return <div className="Movie">
-        <div>
-            <h1>{props.movie.name}</h1>
-            <h1>Released: {props.movie.year}</h1>
-            <h2>Rating: {props.movie.rating}</h2>
-            <h4>{props.movie.image}</h4>
-        </div>
-
+  return (
+    <div className="Movie">
+      <div>
+        <h1>{props.movie.name}</h1>
+      </div>
+      <div>
+        <h1>Released: {props.movie.year}</h1>
+      </div>
+      <div>
+        <h2>Rating: {props.movie.rating}</h2>
+      </div>
+      <div>
+        <h4>{props.movie.image}</h4>
+      </div>
     </div>
-}
+  );
+};
 
-export default Movie
+export default Movie;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,13 +1,12 @@
 import Movie from "./Movie";
 
 const Movies = (props) => {
-
-    return <div className="Movies">
-        
-        {props.movies.map((movie) => {
-            return <Movie movie={movie} key={movie.name}/>
-        })}
-
+  return (
+    <div className="Movies">
+      {props.movies.map((movie) => {
+        return <Movie key={movie.name} movie={movie} />;
+      })}
     </div>
-}
-export default Movies
+  );
+};
+export default Movies;
